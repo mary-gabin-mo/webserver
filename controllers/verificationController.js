fb70bf0c9c832117273ea28514b2a4a92189a468
marymo <maostar1010@gmail.com>
2024-12-07 14:10:30 -0700

    getting a list for unverifiedRequests

--- controllers/verificationController.js
+++ controllers/verificationController.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const getUnverifiedRequests = async (req, res) => {
   try {
     // retrieve all unverified requests
-    const [[unverifiedRequests]] = await Verification.fetchAllUnverified();
+    const [unverifiedRequests] = await Verification.fetchAllUnverified();
     if (!unverifiedRequests)
       return res.status(400).json({ message: "Unverified Requests Not Found" });
     console.log(unverifiedRequests);
